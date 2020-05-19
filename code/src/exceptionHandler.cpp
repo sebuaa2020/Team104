146c4a700e7a97e67f1177a8b30bafa1bc5274fd
郑耀彦 <17373175@buaa.edu.cn>
2020-05-19 18:53:35 +0800

    modify ex & add ignore

--- code/src/exceptionHandler.cpp
+++ code/src/exceptionHandler.cpp
@@ -11,31 +11,37 @@ string getMessage()
 }
 void speechException(string in)
 {
+    ROS_INFO("Failed to get instruction from speech");
     message = "speech exception," + in;
     speak(message);
 }
 void autoExecption(string in)
 {
+    ROS_INFO("Failed to get instruction during auto behavier");
     message = "auto exception, " + in;
     speak(message);
 }
 void grabFailedException(string in)
 {
+    ROS_INFO("Failed to grab things");
     message = "grab failed exception, " + in;
     speak(message);
 }
 void mapFailedException(string in)
 {
+    ROS_INFO("Failed to establish a map");
     message = "map failed exception, " + in;
     speak(message);
 }
 void targetFailedException(string in)
 {
+    ROS_INFO("Failed to walk to the target");
     message = "target failed exception, " + in;
     speak(message);
 }
 void timeLimitedException(string in)
 {
+    ROS_INFO("Walk time limited error");
     message = "time limited exception, " + in;
     speak(message);
 }
