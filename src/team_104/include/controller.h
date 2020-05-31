845deb2d65f300a0d1d919c1f3d6de27d6f3ca9f
pudding <330253798@qq.com>
2020-05-31 16:00:29 +0800

    save

--- src/team_104/include/controller.h
+++ src/team_104/include/controller.h
@@ -1,2 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <tf/transform_listener.h>
+#include <visualization_msgs/Marker.h>
+#include <waterplus_map_tools/Waypoint.h>
+#include <waterplus_map_tools/GetNumOfWaypoints.h>
+#include <waterplus_map_tools/GetWaypointByIndex.h>
+#include <waterplus_map_tools/GetWaypointByName.h>
+#include <string>
